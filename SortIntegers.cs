889e463ba4f6e0f2513a0ddfaeae154dc7cfd690
Eloise Espel <eloise.espel@gmail.com>
2020-09-13 19:57:40 -0700

    Added methods to for default sort

---
+++ SortIntegers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SortArray
+{
+    class SortIntegers
+    {
+        public static void Main()
+        {
+        }
+        static void DisplayArray(string[] arr)
+        {
+            foreach (string a in arr)
+            {
+                Console.Write(a + "\t");
+            }
+        }
+        static void DisplayArray(int[] arr)
+        {
+            foreach (int a in arr)
+            {
+                Console.Write(a + "\t");
+            }
+
+        }
+    }
+}
