2a8e2278fe89c820f29792183843eab31c98b424
Eloise Espel <eloise.espel@gmail.com>
2020-09-13 20:06:00 -0700

    added main method for local testing

--- ArraySorting/ArraySorting/defaultSort.cs
+++ ArraySorting/ArraySorting/defaultSort.cs
@@ -4,6 +4,17 @@ namespace ArraySorting
 {
     class defaultSort
     {
+        public static void Main()
+        {
+            String[] strArray = { "I", "Am", "Learning", "Array", "Sorting", "In", "C#" };
+            int[] intArray = { 23, 76, 12, 43, 90, 30 };
+            Array.Sort(strArray);
+            Array.Sort(intArray);
+            Console.WriteLine("Sorted String Array:\n");
+            DisplayArray(strArray);
+            Console.WriteLine("\n\n\nSorted Integer Array:\n");
+            DisplayArray(intArray);
+        }
         static void DisplayArray(string[] arr)
         {
             foreach (string a in arr)
