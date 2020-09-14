0a3f9f69b205f55a1be3c015dc856756be8c0478
Eloise Espel <eloise.espel@gmail.com>
2020-09-13 20:05:02 -0700

    added code for default array sort

---
+++ ArraySorting/ArraySorting/defaultSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArraySorting
+{
+    class defaultSort
+    {
+        static void DisplayArray(string[] arr)
+        {
+            foreach (string a in arr)
+            {
+                Console.Write(a + "\t");
+            }
+        }
+        static void DisplayArray(int[] arr)
+        {
+            foreach (int a in arr)
+            {
+                Console.Write(a + "\t");
+            }
+        }
+    }
+}
