87ff6b7e0b2a0335476772ede6bf2c52a44eab62
Eloise Espel <eloise.espel@gmail.com>
2020-09-16 09:19:50 -0700

    fixed test method previously was a regular method and fixed error linked to creating the wrong object in customeSort

--- ArraySorting/ArraySortingTests/customeSortTest.cs
+++ ArraySorting/ArraySortingTests/customeSortTest.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ArraySortingTests
 {
+    [TestClass]
     class customeSortTest
     {
-        int[] intArray = { 23, 76, 12, 43, 90, 30 };
-        int[] outArray = { 12, 23, 30, 43, 76, 90 };
-        Array.Sort(intArray, new DescendingComparer());
-        AreEqual(inArray, outArray);
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] inArray = { 23, 76, 12, 43, 90, 30 };
+            int[] outArray = { 12, 23, 30, 43, 76, 90 };
+           // Array.Sort(inArray, new customeSort());
+            Assert.AreEqual(inArray, outArray);
+        }
     }
 }
